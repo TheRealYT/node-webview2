e0dbe3aef2270ce950b93bf820b7a7e4ba26c0d0
TheRealYT <yt040448@gmail.com>
2024-03-22 23:40:13 +0300

    make browser location updatable. !!! But works only once before creating any browser window.

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 using namespace Microsoft::WRL;
 
 const string WINDOW_CLASS = "WindowC++";
+wstring BROWSER_DIR = L"";// L"C:\\Users\\yt040\\Downloads\\Microsoft.WebView2.FixedVersionRuntime.122.0.2365.93.x86";
 
 class BrowserWindow : public Napi::ObjectWrap<BrowserWindow> {
 public:
@@ -27,7 +28,7 @@ public:
 	// Pointer to WebView window
 	wil::com_ptr <ICoreWebView2> webview;
 
-	void send(string eventName) const {
+	void send(string eventName) {
 		auto emitter = this->Value();
 		auto emitFunc = emitter.Get("emit").As<Napi::Function>();
 
@@ -47,7 +48,7 @@ HINSTANCE hInstance = GetModuleHandle(nullptr);
 map<HWND, BrowserWindow*> windows{};
 int windowsCount = 0;
 
-std::string GetLastErrorAsString() {
+string GetLastErrorAsString() {
 	DWORD errorMessageID = GetLastError();
 	if (errorMessageID == 0)
 		return std::string(); // No error message has been recorded
@@ -120,7 +121,7 @@ bool CreateClassWindow() {
 	return true;
 }
 
-static HWND CreateNativeWindow(const string& title) {
+HWND CreateNativeWindow(const string& title) {
 	HWND hWnd = CreateWindow(
 		WINDOW_CLASS.c_str(),
 		title.c_str(),
@@ -147,7 +148,9 @@ void InitializeWebView(HWND hWnd, BrowserWindow* bw, const function<void()> call
 	// <-- WebView2 sample code starts here -->
 	// Step 3 - Create a single WebView within the parent window
 	// Locate the browser and set up the environment for WebView
-	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+	// mklink /d C:\Users\yt040\Downloads\Microsoft.WebView2.FixedVersionRuntime.122.0.2365.93.x86 "C:\Program Files (x86)\Microsoft\Edge\Application\122.0.2365.92"
+
+	CreateCoreWebView2EnvironmentWithOptions(BROWSER_DIR.c_str(), nullptr, nullptr,
 		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
 			[hWnd, bw, callback](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
 
@@ -186,8 +189,7 @@ void InitializeWebView(HWND hWnd, BrowserWindow* bw, const function<void()> call
 							settings->put_AreDefaultContextMenusEnabled(FALSE);
 
 							// Schedule an async task to navigate to Google
-							bw->webview->Navigate(
-								L"http://localhost:5173/");
+							bw->webview->Navigate(L"http://localhost:5173/");
 
 							// <NavigationEvents>
 							// Step 4 - Navigation events
@@ -198,16 +200,10 @@ void InitializeWebView(HWND hWnd, BrowserWindow* bw, const function<void()> call
 									[](ICoreWebView2* webview,
 										ICoreWebView2NavigationStartingEventArgs* args) -> HRESULT {
 											wil::unique_cotaskmem_string uri;
-											args->get_Uri(
-												&uri);
-											std::wstring source(
-												uri.get());
-											if (source.substr(
-												0,
-												4) !=
-												L"http") {
-												args->put_Cancel(
-													true);
+											args->get_Uri(&uri);
+											std::wstring source(uri.get());
+											if (source.substr(0, 4) != L"http") {
+												args->put_Cancel(true);
 											}
 											return S_OK;
 									}).Get(),
@@ -317,6 +313,26 @@ BrowserWindow::BrowserWindow(const Napi::CallbackInfo& info) : Napi::ObjectWrap<
 	throw TypeError::New(env, "Failed to create window");
 }
 
+Value SetBrowserLocation(const CallbackInfo& info)
+{
+	Napi::Env env = info.Env();
+
+	if (info.Length() < 1)
+	{
+		throw TypeError::New(env, "Expected one arg");
+	}
+	else if (!info[0].IsString())
+	{
+		throw TypeError::New(env, "Expected 1st arg to be string");
+	}
+
+	string dir = info[0].As<String>().Utf8Value();
+	wcout << "> " << BROWSER_DIR << endl;
+	BROWSER_DIR = wstring(dir.begin(), dir.end());
+	wcout << "> " << BROWSER_DIR.c_str() << endl;
+	return env.Undefined();
+}
+
 Value MessageLoop(const CallbackInfo& info)
 {
 	MSG msg;
@@ -334,6 +350,7 @@ Value MessageLoop(const CallbackInfo& info)
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
 	exports.Set("BrowserWindow", BrowserWindow::Init(env));
+	exports.Set("SetBrowserLocation", Function::New(env, SetBrowserLocation));
 	exports.Set("MessageLoop", Function::New(env, MessageLoop));
 
 	return exports;
